72df0d7337ee1b5e3b28cdb5342a9344f5dc8f47
mahdi.abedi <mahdi.abedi.dev@gmail.com>
2025-06-26 17:46:28 +0330

    Add Comment section

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm,CommentForm
+from django.views.decorators.http import require_POST
+
 
 # def post_list(request):
 #     post_list = Post.published.all()
@@ -18,7 +20,9 @@ from .forms import EmailPostForm
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form=CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 def post_share(request, post_id):
     #Retrieve post by id
@@ -31,6 +35,19 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
+@require_POST
+def post_comment(request, post_id):
+    post=get_object_or_404(Post, id=post_id,status=Post.Status.PUBLISHED)
+    comment=None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        # Assign the post to the comment then save it
+        comment.post=post
+        comment.save()
+
+    return render(request, 'blog/post/comment.html', {'post':post,'form':form,'comment': comment})
 
 class PostListView(ListView):
     queryset = Post.published.all()
